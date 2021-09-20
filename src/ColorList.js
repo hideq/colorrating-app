328f7f1106153285199d9fd0b9d917b1082b73a7
hideq <hs.show3@gmail.com>
2021-09-20 17:23:34 +0900

    until chapter6.3

---
+++ src/ColorList.js
@@ -0,0 +1,25 @@
+import React, { useContext }from "react"
+import Color from "./Color"
+
+const ColorList = ({ 
+    colors = [], 
+    onRemoveColor = f => f,
+    onRateColor = f => f  
+  }) => {
+  if(!colors.length) return <div>No Colors Listed.(Add a Color)</div>
+  return (
+    <div>
+      {
+        colors.map(color => (
+          <Color 
+            key={color.id} 
+            {...color}  
+            onRemove={onRemoveColor}
+            onRate={onRateColor}
+          />))
+      }
+      </div>
+  )
+}
+
+ export default ColorList
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext } from "react";
+import ReactDOM from "react-dom";
+import colors from "./color-data.json"
+import App from "./App";
+
+const ColorContext = createContext()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ColorContext.Provider value={{ colors }}>
+      <App />
+    </ColorContext.Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default ColorContext
